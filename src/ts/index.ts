cab37b3c415af0862e7822de88f9dd05d6981598
Wahid Halim <wahidkoe@gmail.com>
2025-11-01 12:15:57 +0430

    build pizza creating project finish this secton

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,31 @@
-import { Pizza } from "./models/Pizza";
+import { Pizza, PizzaProps } from "./models/Pizza";
+
+const rootElement = document.querySelector(".root")!;
+
+const createPizzaTemplate = (pizza: PizzaProps): string => {
+  return `
+    <div class="pizza">
+    <h2 >${pizza.title}</h2>
+    <p class="toppings">${pizza.toppings.join(", ")}</p>
+    <p>${pizza.description}</p>
+    <span >$${pizza.price}</span>
+    </div>
+    `;
+};
+
+const renderTemplates = (templates: string[], parent: Element): void => {
+  const templateElements = document.createElement("template");
+
+  for (const t of templates) {
+    templateElements.innerHTML += t;
+  }
+
+  parent.append(templateElements.content);
+};
 
 document.addEventListener("DOMContentLoaded", async () => {
   const pizzas = await Pizza.loadAll();
-  console.log(pizzas);
+
+  const pizzaTemplates = pizzas.map(createPizzaTemplate);
+  renderTemplates(pizzaTemplates, rootElement);
 });
--- src/ts/services/DataResource.ts
+++ src/ts/services/DataResource.ts
@@ -5,7 +5,22 @@ export class DataResource<T> {
     const res = await fetch(this.endpoint);
     return res.json();
   }
-  async loadOne(id: number) {}
-  async delete(id: number) {}
-  async save(data: T) {}
+  async loadOne(id: number): Promise<T> {
+    const res = await fetch(`${this.endpoint}/${id}`);
+    return res.json();
+  }
+  async delete(id: number): Promise<Response> {
+    const res = await fetch(`${this.endpoint}/${id}`, {
+      method: "DELETE",
+    });
+    return res;
+  }
+  async save(data: T): Promise<Response> {
+    const res = await fetch(this.endpoint, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "content-type": "application/json" },
+    });
+    return res;
+  }
 }
